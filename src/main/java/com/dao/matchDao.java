b12e1d0343bb2a85f9f28b4702fc8e91d228e8c0
HiteshChaudhari007 <hiteshc9577@gmail.com>
2024-05-02 12:34:46 +0530

    Initial commit

---
+++ src/main/java/com/dao/matchDao.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import java.util.List;
+
+import com.model.Match;
+
+public interface matchDao {
+
+	List<Match> save();
+
+	List<Match> getLive();
+
+}
---
+++ src/main/java/com/service/matchService.java
@@ -0,0 +1,13 @@
+package com.service;
+
+import java.util.List;
+
+import com.model.Match;
+
+public interface matchService {
+
+	List<Match> save();
+
+	List<Match> getLive();
+
+}
